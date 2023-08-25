cdd3c3d88563e544188619c205788935901fb1ee
dnanto <dnegron2@gmu.edu>
2023-08-24 22:49:44 -0400

    logic for query and ui load

--- bbapp/server.R
+++ bbapp/server.R
@@ -1,45 +1,61 @@
 server <- shinyServer(function(input, output) {
+  match <- eventReactive(input$load, {
+    con <- DBI::dbConnect(RSQLite::SQLite(), "stats.sdb")
+    
+    match <- (
+      DBI::dbGetQuery(
+        con,
+        "
+          SELECT * FROM match
+          WHERE year == ? AND season = ? AND week == ? AND game == ?
+        ",
+        params = lapply(c("year", "season", "week", "game"), \(ele) input[[ele]])
+      ) %>%
+        unlist()
+    )
 
-  output$shot <- renderRHandsontable({
-    fields <- df.ui$shot$field
-    
-    ncol <- length(fields)
-    nrow <- 1
-    result <- (
-      matrix(ncol = ncol, nrow = nrow, dimnames = list(c(), fields)) %>%
-        as_tibble() %>%
-        mutate(across(everything(), as.character))
+    roster <- (
+      DBI::dbGetQuery(
+        con,
+        "
+          SELECT color, roster.* FROM roster
+          JOIN match ON match.id == ? AND (team == team1 OR team == team2)
+          JOIN team ON team == team.id
+          ORDER BY player
+        ",
+        params = match[["id"]]
+      )
     )
+
+    result <- (
+      sapply(
+        c("point", "penalty", "shot"),
+        \(ele)
+        glue::glue_sql(
+          "
+            SELECT color, {`ele`}.* FROM {`ele`}
+            JOIN team ON team == team.id
+            WHERE match == ?
+          ",
+          .con = con
+        ) %>%
+          DBI::dbGetQuery(con, ., params = match[["id"]])
+      ) %>%
+        c(match, roster = roster, .)
+    )
+    DBI::dbDisconnect(con)
     
-    colors <- sort(unique(ls.roster[[paste(year, season, sep = ".")]]$color))
-    players <- ls.roster[[paste(year, season, sep = ".")]]$player
-    
-    rhandsontable(result, width = 1000, height = 1000) %>%
-      hot_col(col = "color", type = "dropdown", source = colors) %>%
-      hot_col(col = "shot", type = "numeric") %>%
-      hot_col(col = "goalie", type = "dropdown", source = players) %>%
-      hot_col(col = "period", type = "numeric") %>%
-      hot_validate_numeric(cols = "shot", min = 0) %>%
-      hot_validate_numeric(cols = "period", min = 1, max = 4)
+    result
   })
-  
+
   output$point <- renderRHandsontable({
-    fields <- df.ui$point$field
-    
-    ncol <- length(fields)
-    nrow <- 1
-    result <- (
-      matrix(ncol = ncol, nrow = nrow, dimnames = list(c(), fields)) %>%
-        as_tibble() %>%
-        mutate(across(everything(), as.character))
-    )
-    
-    colors <- sort(unique(ls.roster[[paste(year, season, sep = ".")]]$color))
-    players <- ls.roster[[paste(year, season, sep = ".")]]$player
-    
-    rhandsontable(result, width = 1000, height = 1000) %>%
-      hot_col(col = "color", type = "dropdown", source = colors) %>%
-      hot_col(col = c("shooter", "assist1", "assist2", "goalie"), type = "dropdown", source = players) %>%
+    data <- match()
+    separate(data$point, time, c("min", "sec"), sep = ":", remove = T) %>%
+      select(all_of(df.ui$point$field)) %>%
+      mutate(across(types, as.logical)) %>%
+      rhandsontable(width = 1000, height = 1000) %>%
+      hot_col(col = "color", type = "dropdown", source = sort(unique(data$roster$color))) %>%
+      hot_col(col = c("shooter", "assist1", "assist2", "goalie"), type = "dropdown", source = data$roster$player) %>%
       hot_col(col = "period", type = "numeric") %>%
       hot_col(col = c("min", "sec"), type = "numeric") %>%
       hot_col(col = types, type = "checkbox") %>%
@@ -48,31 +64,57 @@ server <- shinyServer(function(input, output) {
       hot_validate_numeric(cols = "sec", min = 0, max = 59.999)
   })
 
-output$penalty <- renderRHandsontable({
-  fields <- df.ui$penalty$field
+  output$penalty <- renderRHandsontable({
+    data <- match()
+    separate(data$penalty, time, c("min", "sec"), sep = ":", remove = T) %>%
+      select(all_of(df.ui$penalty$field)) %>%
+      mutate(scored = as.logical(scored)) %>%
+      rhandsontable(width = 1000, height = 1000) %>%
+      hot_col(col = "color", type = "dropdown", source = sort(unique(data$roster$color))) %>%
+      hot_col(col = c("player", "server", "goalie"), type = "dropdown", source = data$roster$player) %>%
+      hot_col(col = "call", type = "dropdown", source = calls) %>%
+      hot_col(col = "duration", type = "numeric") %>%
+      hot_col(col = "period", type = "numeric") %>%
+      hot_col(col = c("min", "sec"), type = "numeric") %>%
+      hot_col(col = "scored", type = "checkbox") %>%
+      hot_validate_numeric(cols = "duration", min = 0) %>%
+      hot_validate_numeric(cols = "period", min = 1, max = 4) %>%
+      hot_validate_numeric(cols = "min", min = 0) %>%
+      hot_validate_numeric(cols = "sec", min = 0, max = 59.999)
+  })
+
+  output$shot <- renderRHandsontable({
+    data <- match()
+    select(data$shot, all_of(df.ui$shot$field)) %>%
+      rhandsontable(width = 1500, height = 1000) %>%
+      hot_col(col = "color", type = "dropdown", source = sort(unique(data$roster$color))) %>%
+      hot_col(col = "SH", type = "numeric") %>%
+      hot_col(col = "goalie", type = "dropdown", source = data$roster$player) %>%
+      hot_col(col = "period", type = "numeric") %>%
+      hot_validate_numeric(cols = "SH", min = 0) %>%
+      hot_validate_numeric(cols = "period", min = 1, max = 4)
+  })
   
-  ncol <- length(fields)
-  nrow <- 1
-  result <- (
-    matrix(ncol = ncol, nrow = nrow, dimnames = list(c(), fields)) %>%
-      as_tibble() %>%
-      mutate(across(everything(), as.character))
-  )
-  
-  colors <- sort(unique(ls.roster[[paste(year, season, sep = ".")]]$color))
-  players <- ls.roster[[paste(year, season, sep = ".")]]$player
-  
-  rhandsontable(result, width = 1000, height = 1000) %>%
-    hot_col(col = "color", type = "dropdown", source = colors) %>%
-    hot_col(col = c("player", "server", "goalie"), type = "dropdown", source = players) %>%
-    hot_col(col = "call", type = "dropdown", source = calls) %>%
-    hot_col(col = "duration", type = "numeric") %>%
-    hot_col(col = "period", type = "numeric") %>%
-    hot_col(col = c("min", "sec"), type = "numeric") %>%
-    hot_col(col = "scored", type = "checkbox") %>%
-    hot_validate_numeric(cols = "duration", min = 0) %>%
-    hot_validate_numeric(cols = "period", min = 1, max = 4) %>%
-    hot_validate_numeric(cols = "min", min = 0) %>%
-    hot_validate_numeric(cols = "sec", min = 0, max = 59.999)
-})
+  output$roster <- renderRHandsontable({
+    data <- match()
+    con <- DBI::dbConnect(RSQLite::SQLite(), "stats.sdb")
+    result <- (
+      DBI::dbGetQuery(
+        con,
+        "
+          SELECT DISTINCT year, season, color, roster.* FROM roster
+          JOIN match ON year = ? AND season = ? AND (team == team1 OR team == team2)
+          JOIN team ON team = team.id
+        ",
+        params = c(data$year, data$season)
+      )
+    )
+    DBI::dbDisconnect(con)
+    mutate(
+      result,
+      season = names(seasons)[season - 21 + 1],
+      across(c("season", "color", "team", "player"), as.factor)
+    ) %>%
+      rhandsontable(width = 1000, height = 1000)
+  })
 })
--- bbapp/ui.R
+++ bbapp/ui.R
@@ -2,16 +2,28 @@ ui <- shinyUI(
   fluidPage(
     sidebarLayout(
       sidebarPanel(
+        numericInput("year", "year", value = max(df.match$year), width = "100%"),
+        selectInput("season", "season", choices = seasons, width = "100%"),
+        numericInput("week", "week", value = 1, min = 1, width = "100%"),
+        numericInput("game", "game", value = 1, min = 1, width = "100%"),
         actionButton("load", "load", width = "100%"),
         actionButton("save", "save", width = "100%"),
-        actionButton("delete", "delete", width = "100%")
+        actionButton("delete", "delete", width = "100%"),
+        width = 2
       ),
       mainPanel(
         tabsetPanel(
-          tabPanel("shot", rHandsontableOutput("shot")),
-          tabPanel("point", rHandsontableOutput("point")),
-          tabPanel("penalty", rHandsontableOutput("penalty")),
-          tabPanel("note")
+          tabPanel(
+            "match",
+            tabsetPanel(
+              tabPanel("point", rHandsontableOutput("point")),
+              tabPanel("penalty", rHandsontableOutput("penalty")),
+              tabPanel("shot", rHandsontableOutput("shot")),
+              tabPanel("note")
+            )
+          ),
+          tabPanel("roster", rHandsontableOutput("roster")),
+          tabPanel("player", rHandsontableOutput("player"))
         )
       )
     )
